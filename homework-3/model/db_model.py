48225c30f238c03aec8cfa12b4e93b8df5d5ebed
İrem Doğancı <45331607+iremdgnc@users.noreply.github.com>
2022-01-25 10:20:59 +0300

    Add files via upload

---
+++ homework-3/model/db_model.py
@@ -0,0 +1,16 @@
+
+class Users:
+
+    def __init__(self, email="None", username="None", name_surname="None", emailuserlk="0",
+                 usernamelk="0", year_of_birth="0000", month_of_birth="00", day_of_birth="00",
+                 country="None", ap="1"):
+        self.email = email
+        self.username = username
+        self.name_surname = name_surname
+        self.emailuserlk = emailuserlk
+        self.usernamelk = usernamelk
+        self.year_of_birth = year_of_birth
+        self.month_of_birth = month_of_birth
+        self.day_of_birth = day_of_birth
+        self.country = country
+        self.ap = ap
